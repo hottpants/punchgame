ecbe69cf8fa5932c4b1d32a9af68f671f455e080
ReidShaffer <59263258+ReidShaffer@users.noreply.github.com>
2025-04-05 17:41:46 -0700

    healthbar shake

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -14,7 +14,10 @@ func _damage_enemy(damage: float):
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 =======
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
 =======

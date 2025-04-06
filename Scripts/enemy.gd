17acbebeaa1fe6ae64020c6a6bccf9f722505056
ReidShaffer <59263258+ReidShaffer@users.noreply.github.com>
2025-04-05 17:39:24 -0700

    healthbar shake

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -9,3 +9,61 @@ func _ready() -> void:
 
 func _damage_enemy(damage: float):
 	$HealthBarViewport/EnemyHealthBar.set_value($HealthBarViewport/EnemyHealthBar.get_value() - damage)
+<<<<<<< Updated upstream
+=======
+	
+	health_shake_state = 0
+	$HealthBarTimer.start()
+	
+
+func _enemy_attack():
+	get_parent().get_node("Player")._lose_health(1)
+	
+func _on_timer_timeout() -> void:
+	print("ASS")
+	_enemy_attack()
+	$Timer.start()
+
+func _shake_healthbar(angle):
+	$EnemyHealthSprite.rotation.z = lerp_angle(rotation.z, deg_to_rad(angle), 0.1)
+
+func _process(delta: float) -> void:
+	match health_shake_state:
+		0:
+			_shake_healthbar(10)
+		1:
+			_shake_healthbar(0)
+		2:
+			_shake_healthbar(-10)
+		3: 
+			_shake_healthbar(0)
+
+
+func _on_health_bar_timer_timeout() -> void:
+	count += 1
+	match health_shake_state:
+		0:
+			health_shake_state = 1
+			print(health_shake_state)
+			$HealthBarTimer.start()
+			
+		1:
+			health_shake_state = 2
+			print(health_shake_state)
+			$HealthBarTimer.start()
+		2:
+			health_shake_state = 3
+			print(health_shake_state)
+			$HealthBarTimer.start()
+		3:
+			health_shake_state = 0
+			print(health_shake_state)
+			$HealthBarTimer.start()
+	if count >= 7:
+		count = 0
+		$HealthBarTimer.stop()
+
+
+func _on_epictimer_timeout() -> void:
+	$HealthBarTimer.start()
+>>>>>>> Stashed changes
